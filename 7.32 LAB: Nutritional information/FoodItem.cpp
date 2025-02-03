5c6b87774b284d71cf7803dc25652a2b7dfdeb66
Christopher Noel <christophernoel@gmail.com>
2025-02-03 16:46:45 -0500

    completed class

--- 7.32 LAB: Nutritional information/FoodItem.cpp
+++ 7.32 LAB: Nutritional information/FoodItem.cpp
@@ -4,41 +4,57 @@
 #include <iomanip>
 #include "FoodItem.h"
 
-using namespace std;
+// default constructor
+FoodItem::FoodItem() {
+    name = "Water";
+    fat = 0.0;
+    carbs = 0.0;
+    protein = 0.0;
+}
 
-// Define default constructor
+// constructor with parameters
+FoodItem::FoodItem(std::string name, double fat, double carbs, double protein) {
+    this->name = name;
+    this->fat = fat;
+    this->carbs = carbs;
+    this->protein = protein;
+}
 
-// Define second constructor with parameters
-// to initialize private data members
-
-string FoodItem::GetName() {
+// Get item name
+std::string FoodItem::GetName() {
     return name;
 }
 
+// Get item fat
 double FoodItem::GetFat() {
     return fat;
 }
 
+// Get item carbs
 double FoodItem::GetCarbs() {
     return carbs;
 }
 
+// Get item protein
 double FoodItem::GetProtein() {
     return protein;
 }
 
+// Get Item Calories
 double FoodItem::GetCalories(double numServings) {
     // Calorie formula
     double calories = ((fat * 9) + (carbs * 4) + (protein * 4)) * numServings;
     return calories;
 }
 
+
+
 void FoodItem::PrintInfo() {
-    cout << fixed << setprecision(2);
-    cout << "Nutritional information per serving of " << name  << ":" << endl;
-    cout << "  Fat: " << fat << " g" << endl;
-    cout << "  Carbohydrates: " << carbs << " g" << endl;
-    cout << "  Protein: " << protein << " g" << endl;
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Nutritional information per serving of " << name  << ":" << std::endl;
+    std::cout << "  Fat: " << fat << " g" << std::endl;
+    std::cout << "  Carbohydrates: " << carbs << " g" << std::endl;
+    std::cout << "  Protein: " << protein << " g" << std::endl;
 }
 
 #include "FoodItem.h"
