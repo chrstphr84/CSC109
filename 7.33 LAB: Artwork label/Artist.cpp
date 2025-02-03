dfe31dce1b50989aaf3d1da1418498c8179567cc
Christopher Noel <christophernoel@gmail.com>
2025-02-03 17:56:52 -0500

    completed classes

--- 7.33 LAB: Artwork label/Artist.cpp
+++ 7.33 LAB: Artwork label/Artist.cpp
@@ -5,14 +5,50 @@
 #include <string>
 using namespace std;
 
-// TODO: Define default constructor
+// Default constructor
+Artist::Artist() {
+    artistName = "unknown";
+    birthYear = -1;
+    deathYear = -1;
+}
 
+// Parameterized constructor
+Artist::Artist(string artistName, int birthYear, int deathYear) {
+    this->artistName = artistName;
+    this->birthYear = birthYear;
+    this->deathYear = deathYear;
+}
 
+// Get the name of the artist
+string Artist::GetName() const {
+    return artistName;
+}
 
-// TODO: Define second constructor to initialize
-//       private fields (artistName, birthYear, deathYear)
+// Get the birth year of the artist
+int Artist::GetBirthYear() const {
+    return birthYear;
+}
 
-// TODO: Define get functions: GetName(), GetBirthYear(), GetDeathYear()
+// Get the death year of the artist
+int Artist::GetDeathYear() const {
+    return deathYear;
+}
 
-// TODO: Define PrintInfo() function
-//      If deathYear is entered as -1, only print birthYear
+// Print the artist information
+    // If the death year is -1, only print the birth year
+    // If the death year is negative, print "present" instead of the death year
+void Artist::PrintInfo() const {
+
+    if (birthYear >= 0) {
+        cout << "Artist: " << artistName << " (" << birthYear;
+        if (deathYear >= 0) {
+            cout << " to " << deathYear << ")" << endl;
+        }
+        else {
+            cout << " to present)" << endl;
+        }
+    }
+    else {
+        cout << "Artist: " << artistName << " (unknown)" << endl;
+    }
+};
--- 7.33 LAB: Artwork label/Artwork.h
+++ 7.33 LAB: Artwork label/Artwork.h
@@ -10,19 +10,15 @@
 class Artwork{
 public:
     Artwork();
-
     Artwork(string title, int yearCreated, Artist artist);
-
     string GetTitle();
-
     int GetYearCreated();
-
     void PrintInfo();
 
 private:
-    // TODO: Declare private data members - title, yearCreated
-
-    // TODO: Declare private data member artist of type Artist
+    string title;
+    int yearCreated;
+    Artist artist;
 
 };
 
