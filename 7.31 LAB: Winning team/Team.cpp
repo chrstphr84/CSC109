c36ab05aae9ce366f43cc6f711c5ed6876354ba6
Christopher Noel <christophernoel@gmail.com>
2025-02-02 23:32:18 -0500

    Initial commit for 7.31 LAB: Winning team

---
+++ 7.31 LAB: Winning team/Team.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Christopher Noel on 2/2/25.
+//
+
+#include "Team.h"
---
+++ 7.31 LAB: Winning team/Team.h
@@ -0,0 +1,16 @@
+//
+// Created by Christopher Noel on 2/2/25.
+//
+
+#ifndef TEAM_H
+#define TEAM_H
+
+
+
+class Team {
+
+};
+
+
+
+#endif //TEAM_H
