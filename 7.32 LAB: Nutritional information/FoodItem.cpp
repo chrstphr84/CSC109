b91449f171a648ee4b4439135bdecaf65cbe1f6f
Christopher Noel <christophernoel@gmail.com>
2025-02-03 01:52:20 -0500

    these are the original files

---
+++ 7.32 LAB: Nutritional information/FoodItem.cpp
@@ -0,0 +1,44 @@
+// Copyright 2025
+
+#include <iostream>
+#include <iomanip>
+#include "FoodItem.h"
+
+using namespace std;
+
+// Define default constructor
+
+// Define second constructor with parameters
+// to initialize private data members
+
+string FoodItem::GetName() {
+    return name;
+}
+
+double FoodItem::GetFat() {
+    return fat;
+}
+
+double FoodItem::GetCarbs() {
+    return carbs;
+}
+
+double FoodItem::GetProtein() {
+    return protein;
+}
+
+double FoodItem::GetCalories(double numServings) {
+    // Calorie formula
+    double calories = ((fat * 9) + (carbs * 4) + (protein * 4)) * numServings;
+    return calories;
+}
+
+void FoodItem::PrintInfo() {
+    cout << fixed << setprecision(2);
+    cout << "Nutritional information per serving of " << name  << ":" << endl;
+    cout << "  Fat: " << fat << " g" << endl;
+    cout << "  Carbohydrates: " << carbs << " g" << endl;
+    cout << "  Protein: " << protein << " g" << endl;
+}
+
+#include "FoodItem.h"
